5b050b104ba57881b430089a4f2bf8759fc119e1
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 14:29:15 +0200

    test

--- js/git.js
+++ js/git.js
@@ -2,17 +2,5 @@ document.querySelector("#my-github-projects").addEventListener("click", function
     event.preventDefault();
     var file = "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1";
     console.log(file)
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET",file,false);
-      rawFile.onreadystatechange = function() {
-          if(rawFile.readyState === 4) {
-              if(rawFile.status === 200 || rawFile.status === 0)
-              {
-                  var allText = rawFile.responseText;
-                  allText.replace(/^.*path.*$/mg, "");
-                  console.log(allText);
-              }
-          }
-      }
-      rawFile.send(null);
+
 }, false);
