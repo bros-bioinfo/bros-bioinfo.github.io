9bbac58ad3645213b7cbbefa41f5eab120dbf408
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 19:29:54 +0200

    list.php

--- js/git.js
+++ js/git.js
@@ -1,6 +1,17 @@
-$.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
-  var text = data;
-});
 
-  var text2 = text.match(/"path": (.+)/g);
-  console.log(text2);
+/*$.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
+
+  var text1 = data;
+  var text2 = String(text1).match(/"path": (.+)/g);
+  var str = String(text2);
+$("#gitlist").text(String(str));
+
+var string = str;
+string.replace(",","<br><br>");
+document.getElementById("gitlist").innerHTML = string;
+console.log(string);
+
+}, 'text');
+
+*/
+$("#gitlist").load("list.php");
