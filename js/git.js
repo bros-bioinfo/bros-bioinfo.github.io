8336eaa03354c04e8c785c8d02e1e5c19ef38bfa
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 15:05:22 +0200

    regex adding

--- js/git.js
+++ js/git.js
@@ -1,4 +1,5 @@
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
   var text = data;
+  data.replace(/"path": (.+)/g);
   console.log(data);
 });
