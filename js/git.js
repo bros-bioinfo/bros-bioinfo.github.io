d7d45ca58dbfecbcbe9a0999637ff40e3f927f55
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 14:32:38 +0200

    test

--- js/git.js
+++ js/git.js
@@ -1,6 +1,4 @@
-document.querySelector("#my-github-projects").addEventListener("click", function(event){
-    event.preventDefault();
-    var file = "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1";
-    console.log(file)
-
-}, false);
+$.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
+  var text = data;
+  console.log(data);
+});
