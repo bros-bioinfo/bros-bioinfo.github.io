1134f60a2cf82a91fe81dc41d716acc9fe54c168
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 14:18:55 +0200

    grab list

--- js/git.js
+++ js/git.js
@@ -1,28 +1,17 @@
-jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
-}
-
-jQuery.fn.loadRepositories = function(username) {
-    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
-
-    var target = this;
-    $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-        sortByName(repos);
-
-        var list = $('<dl/>');
-        target.empty().append(list);
-        $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com')) {
-                list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
-            }
-        });
-      });
-
-    function sortByName(repos) {
-        repos.sort(function(a,b) {
-        return a.name - b.name;
-       });
-    }
-};
+document.querySelector("#my-github-projects").addEventListener("click", function(event){
+    event.preventDefault();
+    var file = "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1";
+    console.log(file)
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET",file,false);
+      rawFile.onreadystatechange = function() {
+          if(rawFile.readyState === 4) {
+              if(rawFile.status === 200 || rawFile.status === 0)
+              {
+                  var allText = rawFile.responseText;
+                  console.log(allText);
+              }
+          }
+      }
+      rawFile.send(null);
+}, false);
