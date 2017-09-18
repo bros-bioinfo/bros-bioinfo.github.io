6033786b92a21bbc28198afe3413b1e57c947eb1
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 15:08:20 +0200

    regex fix 1

--- js/git.js
+++ js/git.js
@@ -1,5 +1,5 @@
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
   var text = data;
-  data.replace(/"path": (.+)/g);
-  console.log(data);
+  var text2 = text.match(/"path": (.+)/g);
+  console.log(text2);
 });
