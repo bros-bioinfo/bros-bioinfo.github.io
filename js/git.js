c79ec50eee4ad30e893395732182a5e0435c5cd8
pierrejacquet <peepanjacquet@gmail.com>
2017-09-18 15:12:24 +0200

    function fix ?

--- js/git.js
+++ js/git.js
@@ -1,5 +1,6 @@
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
   var text = data;
+});
+
   var text2 = text.match(/"path": (.+)/g);
   console.log(text2);
-});
