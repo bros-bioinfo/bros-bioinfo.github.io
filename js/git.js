af2a548583da1098b9d8e1cab3937c2e565f52ba
Pierre JACQUET <peepanjacquet@gmail.com>
2017-09-19 10:13:03 +0200

    updates css readme

--- js/git.js
+++ js/git.js
@@ -1,6 +1,6 @@
-
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
 test = String(data).replace(/^((?!md).)*$/gm,"");
+
 x = 0
 while (x < 80){
 test = test.replace(",","<br>");
@@ -9,10 +9,12 @@ test = test.replace('"','');
 test = test.replace('.md','');
 x++;
 }
-
+//test = test.match(/$/gmi).join("<br>");
+//test = test.replace(/<br>COURS/gmi,'<br><a //href="bros-bioinfo.github.io/COURS');
+//test = test.match(/$/gmi).join('">LIEN</a><br>');
 
 $("#gitlist").html(test+"<br>");
 
-console.log(data+"\n");
+console.log(test+"\n");
 
 }, 'text');
