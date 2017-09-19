e35870e677471350cc2351a97930c3cb2d3306f6
Pierre JACQUET <peepanjacquet@gmail.com>
2017-09-19 08:31:39 +0200

    updates css readme

--- js/git.js
+++ js/git.js
@@ -1,6 +1,6 @@
 
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
-test = String(data).replace(/^((?!md|\.R).)*$/gm,"");
+test = String(data).replace(/^((?!md).)*$/gm,"");
 x = 0
 while (x < 80){
 test = test.replace(",","<br>");
