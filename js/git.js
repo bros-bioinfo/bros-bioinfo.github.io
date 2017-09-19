cd1f9582b6655209ed39db6e9041350ff8afcee3
Pierre JACQUET <peepanjacquet@gmail.com>
2017-09-19 08:06:46 +0200

    updates css readme

--- js/git.js
+++ js/git.js
@@ -1,11 +1,12 @@
 
 $.get( "https://api.github.com/repos/bros-bioinfo/bros-bioinfo.github.io/git/trees/master?recursive=1", function( data ) {
-  data1 = String(data).replace(/^((?!md|\.R).)*$/gm,"");
-//data1 = data.match(/"path": "cours(.+)\n/g);
-var test = String(data1).replace("\n","<br>").replace(',','').replace('"path": "cours/',"").replace('"path":',"").replace('"','').replace('"','').replace(';','').replace("cours","").replace(".md","").replace("<br><br>","<br>");
+test = String(data).replace(/^((?!md|\.R).)*$/gm,"");
 x = 0
 while (x < 80){
-var test = test.replace("\n","<br>").replace(',','').replace('"path": "cours/',"").replace('cours/',"").replace('"path":',"").replace('"','').replace('"','').replace(';','').replace(".md","").replace("<br><br>","<br>").replace("/Algo","Algo");
+test = test.replace(",","<br>");
+test = test.replace('"path": "','');
+test = test.replace('"','');
+test = test.replace('.md','');
 x++;
 }
 
