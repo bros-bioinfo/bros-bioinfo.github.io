4ba95a3123a8f1f2542d855b4f7cd05bebcba421
Pierre Jacquet <piejacquet@emi.u-bordeaux1.fr>
2017-09-22 12:00:34 +0200

    td python1

--- COURS/ALGO_PROG/PROG/TD1/td1.py
+++ COURS/ALGO_PROG/PROG/TD1/td1.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-str = "Je compte jusqu'à 20: "
-n=1
-while (n<21):
-    print str,n
-    if (n == 20):
-        print "  "
-        print "20 ! Ca y est j'arrive!"
-        print "  "
-    n+=1
+25*12 #return 300
+12.4 -3 #return 9.4
+11/4 #return 2
+11.0/4 #return 2.75
+
+i=3
+ #return 3
+
+i=i+2
+ #return 5
+
+j = 0
+ #return 0
+
+print "*Programme Horaire*\n"
+print "\nDonnez l'heure de début de l'experience"
+print "----------------------------------------------\n"
+
+debut = input()
+print "\nL'heure de début selectionnée est: ",debut,"H\n"
+
+print "\nDites à quelle minute s'il vous plait:"
+print "----------------------------------------------\n"
+debutmin = input()
+
+print "\nLes minutes selectionnées sont: ",debutmin,"m\n"
+
+print "\nCombien de minutes dure votre expérience ? :"
+print "----------------------------------------------\n"
+duree = input()
+minrest = duree%60
+heure = duree/60
+
+fin = debut + heure
+print "\nL'heure de fin est:",fin,"H",minrest
