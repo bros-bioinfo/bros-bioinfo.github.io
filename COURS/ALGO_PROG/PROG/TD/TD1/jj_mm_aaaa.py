b9b22504ec805c4e07b18cedd3a3dc4551baa0f5
s3bc40 <s.goncalvesclaro@gmail.com>
2017-10-05 18:52:19 +0200

    update cours

--- COURS/ALGO_PROG/PROG/TD/TD1/jj_mm_aaaa.py
+++ COURS/ALGO_PROG/PROG/TD/TD1/jj_mm_aaaa.py
@@ -15,7 +15,7 @@ month_d = int(month_d)
 years_d = int(years_d)
 
 # Création de liste
-month_T = [1,3,5,7,8,10,12] 
+month_T = [1,3,5,7,8,10,12]
 month_t= [4,6,9,11]
 
 # Calcul de base
@@ -24,23 +24,23 @@ month_f = month_d
 years_f = years_d
 
 # Condtion
-if month_d in month_T and day_f>31:
+err = False
+if (day_d <= 31 and month_d <= 12) and (day_d > 0 and month_d > 0 and years_d > 0): ### Boolean possible ?
+    if month_d in month_T and day_f>31:
         day_f = day_f-31
         month_f = month_d+1
-elif month_f > 12:
-        month_f = monthf-12
-        years_f = years_d+1
-
-if month_d in  month_t and day_f>30:
-        day_f = day_f-30
-        month_f = month_d+1
-elif month_f > 12:
+    elif month_f > 12:
         month_f = month_f-12
         years_f = years_d+1
-
-if month_d == 2 and day_f>28:
+    if month_d in  month_t and day_f>30:
+        day_f = day_f-30
+        month_f = month_d+1
+    elif month_f > 12:
+        month_f = month_f-12
+        years_f = years_d+1
+    if month_d == 2 and day_f>28:
         day_f = day_f-28
         month_f = month_d+1
-
-# Résultat
-print("Date du lendemain : ",day_f,"/",month_f,"/",years_f)
+    print("Date du lendemain : ",day_f,"/",month_f,"/",years_f)
+else:
+    input("Une de vos valeur ne correspond pas aux conditions d'éxecution du programme. Appuyez sur ENTREE...")
