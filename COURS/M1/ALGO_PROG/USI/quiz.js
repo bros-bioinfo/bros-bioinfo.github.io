15e99dc4290802e1d25ce45fc0babd1a1f8aab07
Pierre Jacquet <peepanjacquet@gmail.com>
2017-11-05 10:19:17 +0100

    quizz

--- COURS/M1/ALGO_PROG/USI/quiz.js
+++ COURS/M1/ALGO_PROG/USI/quiz.js
@@ -2,13 +2,16 @@
 //question
 //réponse juste après
 var question = [
-  "Trouver un mot",
+  "Quitter un processus",
+  "exit",
+
+  "Rechercher un motif dans un fichier",
   "grep",
 
   "Remplacer un mot",
   "sed",
 
-  "Afficher les utilisateurs UNIX",
+  "Afficher les utilisateurs UNIX connectés",
   "who",
 
   "Afficher la date",
@@ -18,14 +21,148 @@ var question = [
   "cal",
 
   'Afficher "Bonjour" dans la console',
-  'echo "Bonjour"'
+  'echo "Bonjour"',
+
+//man
+  'Afficher le manuel',
+  "man",
+
+  'Afficher le manuel de la fonction "du" en anglais',
+  "man -L en du",
+
+//du
+  'Afficher la taille occupée sur le disque par chacun des éléments du dossier courant',
+  'du *',
+
+  'Afficher la taille occupée sur le disque par chacun des éléments du dossier courant en omettant les dossiers en ajoutant un suffixe',
+  'du -sh *',
+
+//ls
+  'Afficher le contenu du répertoire dans lequel on se trouve',
+  'ls',
+
+  'Afficher le contenu du répertoire même les fichiers cachés et en ajoutant un caractère à chaque entrée',
+  'ls -aF',
+
+  'Afficher le chemin du répertoire courant',
+  'pwd',
+
+//cd
+  'Aller dans le sous-répertoire "dossier1" ',
+  'cd dossier1',
+
+  'Aller directement au répertoire "home"',
+  'cd',
+
+  'Aller au répertoire parent',
+  'cd ..',
+
+//répertoire...
+  'Créer un répertoire',
+  'mkdir',
+
+  'Supprimer un répertoire',
+  'rmdir',
+
+  'Créer un fichier',
+  'touch',
+
+  "Copie d'un fichier",
+  "cp",
+
+  "Déplacer ou renommer un fichier",
+  "mv",
+
+  "Supprimer un fichier en demandant confirmation",
+  "rm -i",
+
+  "Afficher un fichier page par page",
+  "more",
+
+  "Afficher un fichier au fur et à mesure",
+  "less",
+
+//cat et lecture de fichiers
+  "Lit le contenu d’un fichier et l’affiche dans le terminal",
+  "cat",
+
+  "Déterminer le type d'un fichier",
+  "file",
+
+  "Compter le nombre d'occurences d'un mot",
+  "wc",
+
+  "Afficher la différence entre deux fichiers",
+  "diff",
+
+// processus
+  "Afficher la liste des processus",
+  "ps ux",
+
+  "Tuer un processus",
+  "kill",
+//alias
+
+  "Créer l'alias permettant d'imprimer 'bonjour' dans la console en tapant juste la commande brosbio",
+  'alias brosbio="echo bonjour"',
+
+  "Supprimer l'alias brosbio",
+  "unalias brosbio",
+
+  "Changer les droits",
+  "chmod",
+
+  "Rechercher les fichiers binaires",
+  "whereis",
+
+  "Donner le chemin où se trouve le programme que l'on souhaite exécuter",
+  "which",
+
+  "Trouver un élement de type dossier dans un répertoire",
+  "find -type d",
+
+  "Afficher les processus en cours d’exécution ou les processus stoppés",
+  "jobs",
+
+  "Raccourci clavier permettant de stopper un processus imméditatement sans possibilité d'attendre",
+  "ctrl+z",
+
+  "Raccourci clavier permettant d'envoyer un signal d'arrêt à un processus lui laissant le temps de s'arrêter proprement",
+  "ctrl+c",
+
+  "Place un processus en arrière plan",
+  "bg",
+
+  "Place un processus au premier plan",
+  "fg",
+
+  "Trier",
+  "sort",
+
+  "Créer un lien entre deux fichiers",
+  "ln",
+
+  "Créer un lien symbolique entre deux fichiers",
+  "ln -s",
+
+  "Raccourci permettant de chercher une commande dans l'historique",
+  "ctrl+r",
+
+  "Afficher le code d'erreur/retour d'un processus",
+  "echo $?",
+
+  "Créer un vecteur de variable",
+  "set",
+
+  "Translation d'un caractère par un autre caractère",
+  "tr"
 ];
 
 var range = question.length; //nombre d'élements dans la liste
 var nbquestion = Math.floor(range / 2);
 
 questiondone = [];
-
+note=0;
 
 function randomgif(){
   var gifnum = Math.floor(Math.random() * 4) + 1
@@ -61,6 +198,7 @@ function generatequestion(question) {
   if (questiondone.length >= nbquestion) { //On regarde si le test est fini avant d'aller plus loin
     console.log("TEST FINI !");
     $('#question').text('TEST TERMINE');
+    $('#note').text("Note: "+note+"/"+nbquestion);
     $('#reponse').val(''); //vide la barre
     return;
   }
@@ -70,7 +208,9 @@ function generatequestion(question) {
     number = Math.floor(Math.random() * range / 2) * 2; //génére un nombre impair aléatoire
   }
   numeroquestion = questiondone.length + 1;
-  $('#question').text("(Question "+numeroquestion+"/"+nbquestion+"):    "+question[number]); //écrit la question
+
+  $('#numquestion').text("Question "+numeroquestion+"/"+nbquestion); //écrit la question
+  $('#question').text(question[number]); //écrit la question
 
   questiondone.push(number);
   console.log(questiondone);
@@ -99,7 +239,8 @@ function validating(question, number) {
         console.log(reponse);
         $('#nope').hide();
         $('#yes').show()
-
+        note=note+1
+        $('#note').text("Note: "+note+"/"+nbquestion);
         randomgif();
 
         event.preventDefault();
@@ -111,7 +252,8 @@ function validating(question, number) {
 }
 
 
-
+//INIT HERE
+$('#note').text("Note: "+note+"/"+nbquestion);
 var number = generatequestion(question);
 validating(question, number);
 
