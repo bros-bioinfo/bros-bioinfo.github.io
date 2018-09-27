68de78805fbcdbe543b3a1099b10925b5888e814
Orkeliot <eliot.ragueneau@etu.u-bordeaux.fr>
2018-09-27 14:35:37 +0200

    pep8

--- COURS/M1/SEMESTRE1/ALGO_PROG/PROG/TD/TD1/heure.py
+++ COURS/M1/SEMESTRE1/ALGO_PROG/PROG/TD/TD1/heure.py
@@ -6,7 +6,7 @@ print("Donnez les minutes du début de l'expérience:")
 md = input()
 print("Donnez la durée de l'expérience en minutes:")
 temps = input()
-print("Début de l'expérience :" ,hd,":",md,)
+print("Début de l'expérience :", hd, ":", md,)
 
 
 # Conversion (Python3.x)
@@ -15,19 +15,19 @@ md = int(md)
 temps = int(temps)
 
 # Calcul
-day=0 ### Jour
-temps = temps + md ### Sommes totale en minutes 
-mf = temps % 60 ### Restes en minutes pour la fin
-ht = temps/60 ### Multiple en heure de la durée de l'expérience 
-ht = int(ht) ### Pas de float
-hf = ht + hd ### Heure final 
+day = 0  # Jour
+temps = temps + md  # Sommes totale en minutes
+mf = temps % 60  # Restes en minutes pour la fin
+ht = temps/60  # Multiple en heure de la durée de l'expérience
+ht = int(ht)  # Pas de float
+hf = ht + hd  # Heure final
 
 # Condition (day passé 24h)
 if hf >= 24:
-    day = hf/24 ### Pas de float
+    day = hf/24  # Pas de float
     day = int(day)
-    hf = hf%24
+    hf = hf % 24
     hf = int(hf)
-    
+
 # Résultat
-print("Fin de l'expérience : Jour",day,"Heure :", hf,":",mf)
+print("Fin de l'expérience : Jour", day, "Heure :", hf, ":", mf)
--- COURS/M1/SEMESTRE1/ALGO_PROG/PROG/TD/TD2/td2pierre.py
+++ COURS/M1/SEMESTRE1/ALGO_PROG/PROG/TD/TD2/td2pierre.py
@@ -27,59 +27,59 @@ print """
 """
 
 print "\n\nDonner l'année:"
-annee=input()
+annee = input()
 
 print "\nDonner le mois:"
-imois=raw_input()
-imois=imois.lower()
+imois = raw_input()
+imois = imois.lower()
 
-if imois=="janvier":
-    mois=1
-if imois=="fevrier":
-    mois=2
-if imois=="mars":
-    mois=3
-if imois=="avril":
-    mois=4
-if imois=="mai":
-    mois=5
-if imois=="juin":
-    mois=6
-if imois=="juillet":
-    mois=7
-if imois=="aout":
-    mois=8
-if imois=="septembre":
-    mois=9
-if imois=="octobre":
-    mois=10
-if imois=="novembre":
-    mois=11
-if imois=="decembre":
-    mois=12
-if imois not in ["janvier", "fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]:
+if imois == "janvier":
+    mois = 1
+if imois == "fevrier":
+    mois = 2
+if imois == "mars":
+    mois = 3
+if imois == "avril":
+    mois = 4
+if imois == "mai":
+    mois = 5
+if imois == "juin":
+    mois = 6
+if imois == "juillet":
+    mois = 7
+if imois == "aout":
+    mois = 8
+if imois == "septembre":
+    mois = 9
+if imois == "octobre":
+    mois = 10
+if imois == "novembre":
+    mois = 11
+if imois == "decembre":
+    mois = 12
+if imois not in ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]:
     print "Erreur dans la saisie du mois"
     sys.exit()
 
 print "\nDonner le jour:"
-jour=input()
+jour = input()
 
 print "\nDonnez l'heure de début de l'experience"
 print "------------------------------------------\n"
 
 debut = input()
-print "\nL'heure de début selectionnée est: ",debut,"H\n"
+print "\nL'heure de début selectionnée est: ", debut, "H\n"
 
 print "\nDites à quelle minute s'il vous plait:"
 print "----------------------------------------\n"
 debutmin = input()
 
-print "\nLes minutes selectionnées sont: ",debutmin,"m\n"
+print "\nLes minutes selectionnées sont: ", debutmin, "m\n"
 
 print "\nCombien de minutes dure votre expérience ? :"
 print "----------------------------------------------\n"
 duree = input()
-minrest = (debutmin+duree)%60
+minrest = (debutmin+duree) % 60
 heure = duree/60
 
 fin = debut + heure
@@ -89,46 +89,41 @@ if fin > 23:
     fin = fin % 24
 
 
-
-
-
 if mois == 0 or mois == 2 or mois == 4 or mois == 6 or mois == 9 or mois == 11:
-    if jour>30:
+    if jour > 30:
         print "Erreur dans la saisie du jour (il n'y a pas autant de jours dans ce mois)"
         sys.exit(0)
 
 if mois == 2:
-    if jour>28:
+    if jour > 28:
         print "Erreur dans la saisie du jour (il n'y a pas autant de jours dans ce mois)"
         sys.exit(0)
 
 if mois == 1 or mois == 3 or mois == 5 or mois == 7 or mois == 8 or mois == 10 or mois == 12:
-    if jour>31:
+    if jour > 31:
         print "Erreur dans la saisie du jour (il n'y a pas autant de jours dans ce mois)"
         sys.exit(0)
-
 
 
 journext = jour + journ
 
 
 if mois == 0 or mois == 2 or mois == 4 or mois == 6 or mois == 9 or mois == 11:
-    if journext>31:
-        journext=journext%31
-        mois = mois +1
-
+    if journext > 31:
+        journext = journext % 31
+        mois = mois + 1
 
 
 if mois == 2:
-    if journext>28:
-        journext=journext%28
-        mois = mois +1
+    if journext > 28:
+        journext = journext % 28
+        mois = mois + 1
 
 
 if mois == 1 or mois == 3 or mois == 5 or mois == 7 or mois == 8 or mois == 10:
-    if journext>31:
-        journext=journext%31
-        mois = mois +1
+    if journext > 31:
+        journext = journext % 31
+        mois = mois + 1
 
 if mois == 12:
     if journext > 31:
@@ -137,31 +132,30 @@ if mois == 12:
         annee = annee + 1
 
 
-
 if mois == 1:
-    mois="janvier"
+    mois = "janvier"
 if mois == 2:
-    mois="février"
-if mois == 3 :
-    mois="mars"
+    mois = "février"
+if mois == 3:
+    mois = "mars"
 if mois == 4:
-    mois="avril"
+    mois = "avril"
 if mois == 5:
-    mois="mai"
+    mois = "mai"
 if mois == 6:
-    mois="juin"
+    mois = "juin"
 if mois == 7:
-    mois="juillet"
+    mois = "juillet"
 if mois == 8:
-    mois="aout"
+    mois = "aout"
 if mois == 9:
-    mois="septembre"
+    mois = "septembre"
 if mois == 10:
-    mois="octobre"
+    mois = "octobre"
 if mois == 11:
-    mois="novembre"
+    mois = "novembre"
 if mois == 12:
-    mois="decembre"
+    mois = "decembre"
 
 
-print "\nL'expérience finira à",fin,"H",minrest,"le",journext, mois, annee
+print "\nL'expérience finira à", fin, "H", minrest, "le", journext, mois, annee
